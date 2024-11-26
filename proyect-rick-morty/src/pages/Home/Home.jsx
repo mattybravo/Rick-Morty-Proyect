7bf8b733a5ff0d779083bfc9ad9ab2cbbc17397a
mattybravo <mb7570810@gmail.com>
2024-11-26 19:23:34 -0300

    cambios del proyecto

---
+++ proyect-rick-morty/src/pages/Home/Home.jsx
@@ -0,0 +1,26 @@
+import "./Home.css"
+export default function name() {
+ 
+    
+
+return(
+<div className="d-flex justify-content-center align-item-center">
+
+ <div className="container fondo-blanco p-5">
+ <h1 className="row d-flex justify-content-center">Proyect Rick & Morty</h1>
+  <h2 className="row d-flex justify-content-center">Welcome to Rick & Morty Proyect!</h2> 
+    <p className="row">This proyect was made for practising React and to made a functional website.</p>
+     <p className="row">In this website you can know information of the characters of this animated series.</p>
+      <p className="row">Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion.</p>
+       <p className="row justify-content-center">Lets go!</p>
+      <div className="row">
+        <button className="col-4">Characters</button>
+        <button className="col-4">Contact</button>
+      </div>
+
+  </div>
+
+</div>
+)
+
+}
