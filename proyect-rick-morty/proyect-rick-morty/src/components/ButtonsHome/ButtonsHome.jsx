dea781035a1a5267c53e3f725a2a2a962b89815c
mattybravo <mb7570810@gmail.com>
2025-05-16 23:18:37 -0300

    Corrijo estructura del proyecto, elimino duplicados

--- proyect-rick-morty/proyect-rick-morty/src/components/ButtonsHome/ButtonsHome.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router"
-import "./ButtonsHome.css"
-
-export default function ButtonsHome({contenido}) {
- 
- return(
-    <Link to={contenido} className="btn col-4 botonHome">{contenido}</Link>
- )   
-}
